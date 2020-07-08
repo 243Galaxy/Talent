35205dabbf63e80b52f956509cb53c7f4ba785b4
243Galaxy <60737811+243Galaxy@users.noreply.github.com>
2020-07-08 05:54:47 -0700

    Add files via upload

---
+++ database/migrations/2020_04_23_215758_create_recommendations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecommendationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recommendations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('company');
+            $table->string('position');
+            $table->string('recommendation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recommendations');
+    }
+}
---
+++ database/migrations/2020_04_26_212931_create_talents_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTalentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('talents', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('profession');
+            $table->string('phone');
+            $table->string('state');
+            $table->string('profile');
+            $table->string('image1');
+            $table->string('image2');
+            $table->string('image3');
+            $table->string('project1');
+            $table->string('project2');
+            $table->string('project3');
+            $table->string('location1');
+            $table->string('location2');
+            $table->string('location3');
+            $table->string('year1');
+            $table->string('year2');
+            $table->string('year3');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('talents');
+    }
+}
